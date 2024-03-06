64ad143880694876306dba542ef19ebd8f15faca
taufikhd25 <127413587+taufikhd25@users.noreply.github.com>
2024-03-06 22:15:30 +0700

    Add files

---
+++ oop/Sesi_3/Loop4.java
@@ -0,0 +1,8 @@
+public class Loop4 {
+    
+    public static void main(String[] args) throws Exception{
+        for (int i = 9; i >= -6; i-=3){
+            System.out.println(i);
+        }
+    }
+}
