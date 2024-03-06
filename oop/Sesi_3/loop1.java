b8c23b72726b9f65b2b518e56aa3945e1ad15e45
taufikhd25 <127413587+taufikhd25@users.noreply.github.com>
2024-03-06 22:05:35 +0700

    Add files

---
+++ oop/Sesi_3/loop1.java
@@ -0,0 +1,8 @@
+public class Soal1 {
+    
+    public static void main(String[] args) throws Exception{
+        for (int i=0; i<7; i++){
+            System.out.println("Saya semangat belajar java");
+        }
+    }
+}
