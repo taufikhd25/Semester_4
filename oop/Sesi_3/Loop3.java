6f0ef5409b57aa90a5090b2f73d8e0118517189e
taufikhd25 <127413587+taufikhd25@users.noreply.github.com>
2024-03-06 22:13:58 +0700

    Add files

---
+++ oop/Sesi_3/Loop3.java
@@ -0,0 +1,8 @@
+public class momoland {
+    
+    public static void main(String[] args) throws Exception{
+        for (int i = 9; i >= -6; i-=3){
+            System.out.println(i);
+        }
+    }
+}
