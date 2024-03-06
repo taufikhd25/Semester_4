38ce85be165e917b71f8855f09e0febadc92b839
taufikhd25 <127413587+taufikhd25@users.noreply.github.com>
2024-03-06 22:48:58 +0700

    Add files

---
+++ oop/Sesi_3/Inheritance/App.java
@@ -0,0 +1,9 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        Lingkaran lingkaran = new Lingkaran(7, "Asyikkss");
+        lingkaran.getInfo();
+
+        Segitiga segitiga = new Segitiga(5, 4, "Segitiga ABC");
+        segitiga.getInfo();
+    }
+}
